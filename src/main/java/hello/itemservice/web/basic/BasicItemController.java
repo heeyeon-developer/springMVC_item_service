07ab8114e360f47edbdb4c55e9a78f65730f713b
heeyeonkim <heeyeon3472@gmail.com>
2022-09-25 22:25:41 +0900

    item detail, add item

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -29,6 +28,56 @@ public class BasicItemController {
         model.addAttribute("items", items);
 
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam Integer price,
+                       @RequestParam Integer quantity,
+                       Model model){
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item",item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item){
+//@ModelAttribute 가 하는 일은 자동으로 객체를 생성해주고 생성한 객체가 뷰에서 쓰일겻이기 때문에 model.addAttribute() 도 진행해줌
+
+        itemRepository.save(item);
+//        model.addAttribute("item",item);//@ModelAttribuet() 사용시 생략 가능,Model 도 생략 가능
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item){//@ModelAttribute(name) 을 생략하면 클래스의 이름 첫글자를 소문자로 바꾸어 이름으로 명시됨 Item -> item
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item){//@ModelAttribute 생략 가능하여 생략해도 동일하게 작동!
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     /**
